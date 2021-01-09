8e16007cdcd74b4d91215b86d075efd68bd7826f
guyougula <77035773+guyougula@users.noreply.github.com>
2021-01-09 10:33:18 +0800

    Add files via upload

---
+++ 从键盘输入任意两个整数，求它们的和与积.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	int num1,num2;
+	printf("请输入两个整数:\n");
+	scanf("%d%d",&num1,&num2);
+	printf("两个整数的和为:%d\n",num1+num2);
+	printf("两个整数的积为:%d\n",num1*num2);
+	return 0;
+}
