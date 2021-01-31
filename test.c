78a30081a559051581edb839eb92a608f1ac989e
guyougula <77035773+guyougula@users.noreply.github.com>
2021-01-31 16:19:12 +0800

    Add files via upload

---
+++ test.c
@@ -0,0 +1,82 @@
+#include "三子棋.h"
+  void menu()
+{
+	printf("***************************\n");
+	printf("******1.play    0.exit*****\n");
+	printf("***************************\n");
+}
+//游戏算法的实现
+void game()
+{
+	char ret=0;
+	//数组--存放走出的棋盘信息
+	char board[ROW][COL]={0};
+	InitBoard(board,ROW,COL);//全部是空格
+	//打印棋盘
+	DisplayBoard(board,ROW,COL);
+	//下棋
+	while(1)
+	{
+	//1.玩家走
+		PlayerMove(board,ROW,COL);
+		DisplayBoard(board,ROW,COL);
+		//判断玩家是否赢
+		ret=Iswin(board,ROW,COL);
+		if(ret!='C')
+		{
+			break;
+		}
+	//2.电脑走
+		ComputerMove(board,ROW,COL);
+		DisplayBoard(board,ROW,COL);
+		//判断电脑是否赢
+		ret=Iswin(board,ROW,COL);
+		if(ret!='C')
+		{
+			break;
+		}
+	}
+	if(ret=='*')
+	{
+		printf("玩家赢\n");
+	}
+	else if(ret=='#')
+	{
+		printf("电脑赢\n");
+	}
+	else
+	{
+		printf("平局\n");
+	}
+}
+void test()
+{
+	int input=0;
+	srand((unsigned int)time(NULL));
+	do
+	{
+		menu();
+		printf("请选择>:");
+		scanf("%d",&input);
+		switch(input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("游戏结束\n");
+			break;
+		default:
+			printf("输入错误\n");
+			break;
+		}
+	
+	}
+	while(input);
+}
+int main()
+{
+	test();
+	return 0;
+}
+ 
